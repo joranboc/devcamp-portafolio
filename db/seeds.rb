ba41c122695e0d9a52a25f8f04f15d725762c1dc
Joranboc <joranboc@hotmail.com>
2018-08-04 20:22:08 -0500

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,3 +42,11 @@ end
   )
 end
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Technology.create!(
+    name: "Technology #{technology}",
+    portfolio_id: Portfolio.last.id
+  )
+end
+puts "3 Technologies created"
